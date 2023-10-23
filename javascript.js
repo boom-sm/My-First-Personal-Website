68251b3bf483fcf90852153c840345a76e7b3378
Santiparp Mekmueangthong <boom@Santiparps-MacBook-Pro.local>
2023-10-23 01:12:39 -0400

    adding the images

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//Have a html page to display message Hallo World
+//in javascript file ill write message 
+//ill display the message on html and alert on the page 
+
+//This is the Message
+let code = 'Hello World!!!'
+
+//Alert
+alert(code);
